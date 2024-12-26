c11e38ba26eb8fba73f01ee7514ed6ccce7d7ec0
tuyucheng <tuyucheng2000@163.com>
2024-12-26 09:59:56 +0800

    Spring Boot 3.4 Structured Logging

---
+++ spring-boot-modules/spring-boot-3-4/src/main/java/cn/tuyucheng/taketoday/springstructuredlogging/StructuredLoggingApp.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.springstructuredlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredLoggingApp {
+
+   public static void main(String[] args) {
+      SpringApplication.run(StructuredLoggingApp.class, args);
+   }
+}
