7b9e680ad6c56a8861e3acd156b6a50c1da80c88
tuyucheng <tuyucheng2000@163.com>
2025-01-06 10:33:50 +0800

    Reactive kafka stream

---
+++ spring-reactive-modules/spring-reactive-kafka-stream-binder/src/main/java/cn/tuyucheng/taketoday/reactive/kafka/stream/binder/api/StocksApi.java
@@ -0,0 +1,34 @@
+package cn.tuyucheng.taketoday.reactive.kafka.stream.binder.api;
+
+import cn.tuyucheng.taketoday.reactive.kafka.stream.binder.domain.StockUpdate;
+import cn.tuyucheng.taketoday.reactive.kafka.stream.binder.repository.ClickHouseRepository;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+
+import java.time.Instant;
+
+@RestController
+public class StocksApi {
+
+   private final ClickHouseRepository repository;
+
+   @Autowired
+   public StocksApi(ClickHouseRepository repository) {
+      this.repository = repository;
+   }
+
+   @GetMapping("/stock-prices-out")
+   public Flux<StockUpdate> getAvgStockPrices(@RequestParam("from") @NotNull Instant from, @RequestParam("to") @NotNull Instant to) {
+      if (from.isAfter(to)) {
+         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "'from' must come before 'to'");
+      }
+
+      return repository.findMinuteAvgStockPrices(from, to);
+   }
+}
---
+++ spring-reactive-modules/spring-reactive-kafka-stream-binder/src/main/java/cn/tuyucheng/taketoday/reactive/kafka/stream/binder/domain/StockUpdate.java
@@ -0,0 +1,6 @@
+package cn.tuyucheng.taketoday.reactive.kafka.stream.binder.domain;
+
+import java.time.Instant;
+
+public record StockUpdate(String symbol, double price, String currency, Instant timestamp) {
+}
