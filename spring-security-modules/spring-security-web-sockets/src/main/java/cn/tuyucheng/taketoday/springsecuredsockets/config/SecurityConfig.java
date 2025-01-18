3244420fe449aeaa00823927f15e37ddef4d143d
tuyucheng <tuyucheng2000@163.com>
2025-01-18 10:19:34 +0800

    Update spring-security-websockets article

--- spring-security-modules/spring-security-web-sockets/src/main/java/cn/tuyucheng/taketoday/springsecuredsockets/config/SecurityConfig.java
+++ spring-security-modules/spring-security-web-sockets/src/main/java/cn/tuyucheng/taketoday/springsecuredsockets/config/SecurityConfig.java
@@ -74,17 +74,20 @@ public class SecurityConfig {
     */
    @Bean
    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                  authorizationManagerRequestMatcherRegistry
-                        .requestMatchers("/", "/index", "/authenticate").permitAll()
-                        .requestMatchers("/secured/**/**", "/secured/**/**/**", "/secured/socket", "/secured/success").authenticated()
-                        .anyRequest().authenticated())
-            .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login").permitAll()
+      http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers("/", "/index", "/authenticate")
+                  .permitAll()
+                  .requestMatchers("/secured/**/**", "/secured/**/**/**", "/secured/socket", "/secured/success")
+                  .authenticated()
+                  .anyRequest()
+                  .authenticated())
+            .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login")
+                  .permitAll()
                   .usernameParameter("username")
                   .passwordParameter("password")
                   .loginProcessingUrl("/authenticate")
                   .successHandler(loginSuccessHandler())
-                  .failureUrl("/denied").permitAll())
+                  .failureUrl("/denied")
+                  .permitAll())
             .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessHandler(logoutSuccessHandler()))
             /**
              * Applies to User Roles - not to login failures or unauthenticated access attempts.
@@ -96,7 +99,7 @@ public class SecurityConfig {
       http.csrf(AbstractHttpConfigurer::disable);
 
       /** This is solely required to support H2 console viewing in Spring MVC with Spring Security */
-      http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+      http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
             .authorizeHttpRequests(Customizer.withDefaults());
       return http.build();
    }
