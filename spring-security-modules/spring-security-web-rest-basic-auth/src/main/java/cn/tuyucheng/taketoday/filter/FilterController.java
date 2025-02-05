5b690d27044e11c907a2405f78b25d3c487cc583
tuyucheng <tuyucheng2000@163.com>
2025-02-05 23:12:22 +0800

    Apply Spring Security Filter Only on Secured Endpoints

---
+++ spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/cn/tuyucheng/taketoday/filter/FilterController.java
@@ -0,0 +1,18 @@
+package cn.tuyucheng.taketoday.filter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilterController {
+
+   @GetMapping("/securityNone/test")
+   public String publicEndpoint() {
+      return "This is a public endpoint.";
+   }
+
+   @GetMapping("/secured/test")
+   public String securedEndpoint() {
+      return "This is a secured endpoint.";
+   }
+}
