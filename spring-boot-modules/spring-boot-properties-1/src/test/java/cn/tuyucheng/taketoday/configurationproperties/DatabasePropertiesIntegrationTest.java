ea51969ef10cca64ad5406b2e30fd18da921db56
tuyucheng <tuyucheng2000@163.com>
2025-02-06 11:52:38 +0800

    Check Article Code Matches GitHub

--- spring-boot-modules/spring-boot-properties-1/src/test/java/cn/tuyucheng/taketoday/configurationproperties/DatabasePropertiesIntegrationTest.java
+++ spring-boot-modules/spring-boot-properties-1/src/test/java/cn/tuyucheng/taketoday/configurationproperties/DatabasePropertiesIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DatabaseConfigPropertiesApp.class)
 @TestPropertySource("classpath:database-test.properties")
