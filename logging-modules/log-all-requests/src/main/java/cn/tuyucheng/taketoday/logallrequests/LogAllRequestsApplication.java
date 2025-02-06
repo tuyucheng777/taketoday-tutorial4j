70b6ea4320339ae8e6ae0bd10eb4a121843d3e94
tuyucheng <tuyucheng2000@163.com>
2025-02-06 11:57:52 +0800

    How to log all requests and responses with an exception in a single place

---
+++ logging-modules/log-all-requests/src/main/java/cn/tuyucheng/taketoday/logallrequests/LogAllRequestsApplication.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.logallrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAllRequestsApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(LogAllRequestsApplication.class, args);
+   }
+}
