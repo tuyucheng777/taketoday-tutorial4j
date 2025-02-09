9887d8f15042920aea6646ae898ad77ffffe78d4
tuyucheng <tuyucheng2000@163.com>
2025-02-10 03:17:42 +0800

    profile combinations

---
+++ spring-boot-modules/spring-boot-environment/src/main/java/cn/tuyucheng/taketoday/profiles/ThreeProfilesComponent.java
@@ -0,0 +1,9 @@
+package cn.tuyucheng.taketoday.profiles;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("a & b & c")
+public class ThreeProfilesComponent {
+}
