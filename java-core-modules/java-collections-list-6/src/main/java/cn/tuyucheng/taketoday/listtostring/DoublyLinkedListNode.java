284c03066e8ab6126bd3aa3a4c5ba8bc35fa86c0
tuyucheng <tuyucheng2000@163.com>
2025-02-12 11:04:22 +0800

    Printing out a Linked List Using toString()

---
+++ java-core-modules/java-collections-list-6/src/main/java/cn/tuyucheng/taketoday/listtostring/DoublyLinkedListNode.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.listtostring;
+
+public class DoublyLinkedListNode {
+   Integer data;
+   DoublyLinkedListNode next;
+   DoublyLinkedListNode prev;
+
+   DoublyLinkedListNode(Integer data) {
+      this.data = data;
+   }
+}
