da232d95c4b0f4e59db54522515f9b7d5686f32e
tuyucheng <tuyucheng2000@163.com>
2025-02-27 18:46:23 +0800

    exploring model context protocol with spring ai

---
+++ spring-boot-modules/spring-boot-ai-2/src/main/java/cn/tuyucheng/taketoday/springai/mcp/client/ChatbotService.java
@@ -0,0 +1,22 @@
+package cn.tuyucheng.taketoday.springai.mcp.client;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+class ChatbotService {
+
+   private final ChatClient chatClient;
+
+   ChatbotService(ChatClient chatClient) {
+      this.chatClient = chatClient;
+   }
+
+   String chat(String question) {
+      return chatClient
+            .prompt()
+            .user(question)
+            .call()
+            .content();
+   }
+}
