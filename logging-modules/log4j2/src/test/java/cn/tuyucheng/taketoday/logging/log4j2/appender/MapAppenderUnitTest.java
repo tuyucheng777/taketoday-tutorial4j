b4155da7e670cc9f65305925e8073a8a25380b83
tuyucheng <tuyucheng2000@163.com>
2025-02-27 18:47:49 +0800

    Converted unit to integration test

--- logging-modules/log4j2/src/test/java/cn/tuyucheng/taketoday/logging/log4j2/appender/MapAppenderUnitTest.java
+++ logging-modules/log4j2/src/test/java/cn/tuyucheng/taketoday/logging/log4j2/appender/MapAppenderUnitTest.java
@@ -12,13 +12,13 @@ import org.junit.runners.JUnit4;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
-public class MapAppenderIntegrationTest {
+public class MapAppenderUnitTest {
 
    private Logger logger;
 
    @Before
    public void setup() {
-      logger = LogManager.getLogger(MapAppenderIntegrationTest.class);
+      logger = LogManager.getLogger(MapAppenderUnitTest.class);
    }
 
    @Test
