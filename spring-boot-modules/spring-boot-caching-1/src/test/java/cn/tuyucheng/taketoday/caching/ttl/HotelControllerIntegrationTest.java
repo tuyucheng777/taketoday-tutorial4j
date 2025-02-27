df3f11efa1419e32d8377f1d63914bf172dd834e
tuyucheng <tuyucheng2000@163.com>
2025-02-27 18:41:00 +0800

    Fixed integration

--- spring-boot-modules/spring-boot-caching-1/src/test/java/cn/tuyucheng/taketoday/caching/ttl/HotelControllerIntegrationTest.java
+++ spring-boot-modules/spring-boot-caching-1/src/test/java/cn/tuyucheng/taketoday/caching/ttl/HotelControllerIntegrationTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:data-hotel.sql")
 @SlowTest
