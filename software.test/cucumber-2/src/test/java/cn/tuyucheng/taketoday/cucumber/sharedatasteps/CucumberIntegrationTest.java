4bda7e26584a1d3327fb43810ff33b516e3d7889
tuyucheng <tuyucheng2000@163.com>
2025-03-04 21:29:05 +0800

    fixed the package errors

--- software.test/cucumber-2/src/test/java/cn/tuyucheng/taketoday/cucumber/sharedatasteps/CucumberIntegrationTest.java
+++ software.test/cucumber-2/src/test/java/cn/tuyucheng/taketoday/cucumber/sharedatasteps/CucumberIntegrationTest.java
@@ -2,9 +2,6 @@ package cn.tuyucheng.taketoday.cucumber.sharedatasteps;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.cucumber.spring.CucumberContextConfiguration;
-
-@CucumberContextConfiguration
 @SpringBootTest
 class CucumberIntegrationTest {
 }
