f9261f80d079edd76c9d3a0784375419582a860f
tuyucheng <tuyucheng2000@163.com>
2025-03-12 13:09:29 +0800

    Create InterceptingClientHttpRequestTest.java  Support Request Attributes in RestClient

---
+++ spring-boot-modules/spring-boot-3-2/src/test/java/cn/tuyucheng/taketoday/restclient/InterceptingClientHttpRequestTest.java
@@ -0,0 +1,22 @@
+package cn.tuyucheng.taketoday.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class InterceptingClientHttpRequestTest {
+
+   @Test
+   void updateRequestAttribute() throws Exception {
+      String attrName = "attr1";
+      String attrValue = "value1";
+
+      assertDoesNotThrow(() -> {
+         ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+            request.getAttributes().put(attrName, attrValue);
+            return execution.execute(request, body);
+         };
+      });
+   }
+}
