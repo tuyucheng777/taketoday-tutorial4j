bfe38afe0c6a9819802d99708bd5c032dd0cb4e7
tuyucheng <tuyucheng2000@163.com>
2025-03-16 11:57:15 +0800

    Update article "Spring Events"

---
+++ spring-modules/spring-core-1/src/main/java/cn/tuyucheng/taketoday/springevents/asynchronous/AnnotationDrivenEventListener.java
@@ -0,0 +1,29 @@
+package cn.tuyucheng.taketoday.springevents.asynchronous;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.scheduling.annotation.Async;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+
+import cn.tuyucheng.taketoday.springevents.synchronous.CustomSpringEvent;
+
+@Component
+public class AnnotationDrivenEventListener {
+
+   @EventListener
+   @Async
+   public void handleAsyncEvent(CustomSpringEvent event) {
+      System.out.println("Handle event asynchronously: " + event.getMessage());
+   }
+
+   @Async
+   @Transactional(propagation = REQUIRES_NEW)
+   @TransactionalEventListener
+   void handleCustom(CustomSpringEvent event) {
+      /* … */
+   }
+}
