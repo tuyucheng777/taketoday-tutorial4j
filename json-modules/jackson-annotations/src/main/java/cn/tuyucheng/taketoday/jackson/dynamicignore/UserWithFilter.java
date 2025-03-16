6f55cba2cc4b714036031b5e5055dda21c8cc957
tuyucheng <tuyucheng2000@163.com>
2025-03-16 22:12:17 +0800

    Dynamic Ignore with Jackson

---
+++ json-modules/jackson-annotations/src/main/java/cn/tuyucheng/taketoday/jackson/dynamicignore/UserWithFilter.java
@@ -0,0 +1,27 @@
+package cn.tuyucheng.taketoday.jackson.dynamicignore;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+@JsonFilter("publicFilter")
+public class UserWithFilter {
+
+   private Long id;
+
+   private String name;
+
+   public UserWithFilter(Long id, String name) {
+      this.id = id;
+      this.name = name;
+   }
+
+   public UserWithFilter() {
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public Long getId() {
+      return id;
+   }
+}
---
+++ json-modules/jackson-annotations/src/main/java/cn/tuyucheng/taketoday/jackson/dynamicignore/UserWithMixin.java
@@ -0,0 +1,35 @@
+package cn.tuyucheng.taketoday.jackson.dynamicignore;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class UserWithMixin {
+
+   private Long id;
+
+   private String name;
+
+   public UserWithMixin(Long id, String name) {
+      this.id = id;
+      this.name = name;
+   }
+
+   public UserWithMixin() {
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   /**
+    * Mixin interface that is used to hide sensitive information
+    */
+   public interface PublicMixin {
+
+      @JsonIgnore
+      Long getId();
+   }
+}
