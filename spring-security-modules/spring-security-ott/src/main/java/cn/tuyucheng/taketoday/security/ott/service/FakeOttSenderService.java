2d0cc6dc501a6df383be41ec49f03cc98597b0d8
tuyucheng <tuyucheng2000@163.com>
2025-03-24 20:53:24 +0800

    fix

--- spring-security-modules/spring-security-ott/src/main/java/cn/tuyucheng/taketoday/security/ott/service/FakeOttSenderService.java
+++ spring-security-modules/spring-security-ott/src/main/java/cn/tuyucheng/taketoday/security/ott/service/FakeOttSenderService.java
@@ -15,7 +15,7 @@ public class FakeOttSenderService implements OttSenderService {
    @Override
    public void sendTokenToUser(String username, String token, Instant expiresAt) {
       lastTokenByUser.put(username, token);
-      log.info("Sending token to username '{}'. token={}, expiresAt={}", username, token, expiresAt);
+      LOGGER.info("Sending token to username '{}'. token={}, expiresAt={}", username, token, expiresAt);
    }
 
    @Override
