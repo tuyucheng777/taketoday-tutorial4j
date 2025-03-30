889a000a44b162920bc56a33317b1c505ba67807
tuyucheng <tuyucheng2000@163.com>
2025-03-30 20:37:14 +0800

    Add various approaches for copying specific fields

---
+++ spring-modules/spring-core-4/src/main/java/cn/tuyucheng/taketoday/copyproperties/BeanUtilsCopyProperties.java
@@ -0,0 +1,18 @@
+package cn.tuyucheng.taketoday.copyproperties;
+
+import org.springframework.beans.BeanUtils;
+
+import java.beans.PropertyDescriptor;
+import java.util.Arrays;
+import java.util.Set;
+
+public class BeanUtilsCopyProperties {
+   public static void copySelectedPropertiesUsingCustomWrapper(Object source, Object target, Set<String> props) {
+      String[] excludedProperties = Arrays.stream(BeanUtils.getPropertyDescriptors(source.getClass()))
+            .map(PropertyDescriptor::getName)
+            .filter(name -> !props.contains(name))
+            .toArray(String[]::new);
+
+      BeanUtils.copyProperties(source, target, excludedProperties);
+   }
+}
