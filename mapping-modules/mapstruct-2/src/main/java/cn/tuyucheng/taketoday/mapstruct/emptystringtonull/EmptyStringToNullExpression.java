65547b84f1c74d87efb5a41a6d1ddac31545822b
tuyucheng <tuyucheng2000@163.com>
2025-04-29 17:07:45 +0800

    Map empty string to null in MapStruct

---
+++ mapping-modules/mapstruct-2/src/main/java/cn/tuyucheng/taketoday/mapstruct/emptystringtonull/EmptyStringToNullExpression.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.mapstruct.emptystringtonull;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface EmptyStringToNullExpression {
+   EmptyStringToNullExpression INSTANCE = Mappers.getMapper(EmptyStringToNullExpression.class);
+
+   @Mapping(target = "lastName", expression = "java(student.lastName.isEmpty() ? null : student.lastName)")
+   Teacher toTeacher(Student student);
+}
---
+++ mapping-modules/mapstruct-2/src/main/java/cn/tuyucheng/taketoday/mapstruct/emptystringtonull/Student.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.mapstruct.emptystringtonull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+   String firstName;
+   String lastName;
+}
