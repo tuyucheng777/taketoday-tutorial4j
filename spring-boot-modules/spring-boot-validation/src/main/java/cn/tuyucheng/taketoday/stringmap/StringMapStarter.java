026df7f03efcfa44a4696ec92a859d427ea70304
tuyucheng <tuyucheng2000@163.com>
2025-05-01 21:50:38 +0800

    Validate Map<String, String> using Spring Validator

---
+++ spring-boot-modules/spring-boot-validation/src/main/java/cn/tuyucheng/taketoday/stringmap/StringMapStarter.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.stringmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringMapStarter {
+   public static void main(String[] args) {
+      SpringApplication.run(StringMapStarter.class, args);
+   }
+}
