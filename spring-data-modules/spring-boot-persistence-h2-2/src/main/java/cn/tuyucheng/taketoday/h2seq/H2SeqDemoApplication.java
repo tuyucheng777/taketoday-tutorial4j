fde3ff2754dadfc96b5ec611c7c0c4c118a8006f
tuyucheng <tuyucheng2000@163.com>
2025-08-23 22:03:02 +0800

    h2 sequence

---
+++ spring-data-modules/spring-boot-persistence-h2-2/src/main/java/cn/tuyucheng/taketoday/h2seq/H2SeqDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.h2seq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class H2SeqDemoApplication {
+
+   public static void main(String... args) {
+      SpringApplication.run(H2SeqDemoApplication.class, args);
+   }
+}
