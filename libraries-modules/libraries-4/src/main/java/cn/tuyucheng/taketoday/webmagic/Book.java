a1df1141f181b2880f23267ff8bae7a41fcde8fe
tuyucheng <tuyucheng2000@163.com>
2025-08-23 21:54:25 +0800

    Add code for Web Crawler using WebMagic Library

---
+++ libraries-modules/libraries-4/src/main/java/cn/tuyucheng/taketoday/webmagic/Book.java
@@ -0,0 +1,19 @@
+package cn.tuyucheng.taketoday.webmagic;
+
+public class Book {
+   private final String title;
+   private final String price;
+
+   public Book(String title, String price) {
+      this.title = title;
+      this.price = price;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public String getPrice() {
+      return price;
+   }
+}
