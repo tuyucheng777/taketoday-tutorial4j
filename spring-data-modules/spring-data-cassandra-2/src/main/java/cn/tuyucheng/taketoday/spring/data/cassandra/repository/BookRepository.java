da330f744480145ddc155b4eb5e45ee60eca57b8
tuyucheng <tuyucheng2000@163.com>
2025-08-23 21:36:44 +0800

    Move spring-data-cassandratemplate-cqltemplate article from spring-data-cassandra-1 to spring-data-cassandra-2

---
+++ spring-data-modules/spring-data-cassandra-2/src/main/java/cn/tuyucheng/taketoday/spring/data/cassandra/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.spring.data.cassandra.repository;
+
+import java.util.UUID;
+
+import cn.tuyucheng.taketoday.spring.data.cassandra.model.Book;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CassandraRepository<Book, UUID> {
+
+   Iterable<Book> findByTitleAndPublisher(String title, String publisher);
+}
