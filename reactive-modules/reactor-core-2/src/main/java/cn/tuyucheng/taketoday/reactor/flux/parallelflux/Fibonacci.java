8b502327dbcd7d247d64d27537577a72a032891c
tuyucheng <tuyucheng2000@163.com>
2025-08-23 21:43:51 +0800

    Parallel Flux vs Flux in Project Reactor

---
+++ reactive-modules/reactor-core-2/src/main/java/cn/tuyucheng/taketoday/reactor/flux/parallelflux/Fibonacci.java
@@ -0,0 +1,8 @@
+package cn.tuyucheng.taketoday.reactor.flux.parallelflux;
+
+public class Fibonacci {
+   public static long fibonacci(int n) {
+      if (n <= 1) return n;
+      return fibonacci(n - 1) + fibonacci(n - 2);
+   }
+}
