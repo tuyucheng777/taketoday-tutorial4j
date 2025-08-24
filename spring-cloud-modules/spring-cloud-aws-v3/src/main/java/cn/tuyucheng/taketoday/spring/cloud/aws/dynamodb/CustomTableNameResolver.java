3bf8c1bbd36950ed32839aac8c38bd203ca00f3f
tuyucheng <tuyucheng2000@163.com>
2025-08-24 15:34:00 +0800

    integrating amazon dynamodb with spring boot using spring-cloud-aws

---
+++ spring-cloud-modules/spring-cloud-aws-v3/src/main/java/cn/tuyucheng/taketoday/spring/cloud/aws/dynamodb/CustomTableNameResolver.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.spring.cloud.aws.dynamodb;
+
+import io.awspring.cloud.dynamodb.DynamoDbTableNameResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+class CustomTableNameResolver implements DynamoDbTableNameResolver {
+
+   @Override
+   public <T> String resolve(Class<T> clazz) {
+      return clazz.getAnnotation(TableName.class).name();
+   }
+}
---
+++ spring-cloud-modules/spring-cloud-aws-v3/src/main/java/cn/tuyucheng/taketoday/spring/cloud/aws/dynamodb/TableName.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.spring.cloud.aws.dynamodb;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+@interface TableName {
+   String name();
+}
