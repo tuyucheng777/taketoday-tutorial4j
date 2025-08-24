ea302c6be990a9920870583ac1a89e8ff125e565
tuyucheng <tuyucheng2000@163.com>
2025-08-24 15:24:10 +0800

    added code

---
+++ spring-security-modules/spring-security-faking-oauth2-sso/src/main/java/cn/tuyucheng/taketoday/fakingouath2sso/FakingOauth2SsoApplication.java
@@ -0,0 +1,20 @@
+package cn.tuyucheng.taketoday.fakingouath2sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FakingOauth2SsoApplication {
+
+   @GetMapping("/")
+   public String get() {
+      return "Login Success";
+   }
+
+   public static void main(String[] args) {
+      SpringApplication.run(FakingOauth2SsoApplication.class, args);
+   }
+}
