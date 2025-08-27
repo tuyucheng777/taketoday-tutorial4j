611df4da5450586ea247bf6a39b8be3302e3bb58
tuyucheng <tuyucheng2000@163.com>
2025-08-27 10:35:18 +0800

    Added unit test for Quarkus Panache

---
+++ quarkus-modules/quarkus-panache/src/main/java/cn/tuyucheng/taketoday/quarkus/Article.java
@@ -0,0 +1,21 @@
+package cn.tuyucheng.taketoday.quarkus;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Article extends PanacheEntity {
+
+   public String title;
+   public String content;
+   public String status;
+
+   public Article() {
+   }
+
+   public Article(String title, String content, String status) {
+      this.title = title;
+      this.content = content;
+      this.status = status;
+   }
+}
