bcf31ae66b8fa197b086d90bc87c918f6e55f48f
tuyucheng <tuyucheng2000@163.com>
2025-08-27 10:33:00 +0800

    Add code for intro to spring grpc

---
+++ spring-modules/spring-grpc/src/main/java/cn/tuyucheng/taketoday/grpc/SpringgRPCApplication.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgRPCApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(SpringgRPCApplication.class, args);
+   }
+}
