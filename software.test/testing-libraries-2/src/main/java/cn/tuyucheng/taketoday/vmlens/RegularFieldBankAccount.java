703339a0e1f17f191fd93429ca65037cf962f2b2
tuyucheng <tuyucheng2000@163.com>
2025-08-29 17:41:31 +0800

    examples for the blog article Unit Tests for Concurrent Java with VMLens

---
+++ software.test/testing-libraries-2/src/main/java/cn/tuyucheng/taketoday/vmlens/RegularFieldBankAccount.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.vmlens;
+
+public class RegularFieldBankAccount {
+
+   private int amount;
+
+   public int getAmount() {
+      return amount;
+   }
+
+   public void update(int delta) {
+      amount += delta;
+   }
+}
---
+++ software.test/testing-libraries-2/src/main/java/cn/tuyucheng/taketoday/vmlens/VolatileFieldBankAccount.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.vmlens;
+
+public class VolatileFieldBankAccount {
+
+   private volatile int amount;
+
+   public int getAmount() {
+      return amount;
+   }
+
+   public void update(int delta) {
+      amount += delta;
+   }
+}
