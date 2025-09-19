be3f8bdab09b4636f737df4d54543ac301733ad1
tuyucheng <tuyucheng2000@163.com>
2025-09-19 22:08:09 +0800

    fix

--- spring-cloud-modules/spring-cloud-gateway-2/src/main/java/cn/tuyucheng/taketoday/springcloudgateway/GatewayApplication.java
+++ spring-cloud-modules/spring-cloud-gateway-2/src/main/java/cn/tuyucheng/taketoday/springcloudgateway/GatewayApplication.java
@@ -2,12 +2,11 @@ package cn.tuyucheng.taketoday.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
+   static void main(String[] args) {
+      SpringApplication.run(GatewayApplication.class, args);
+   }
 }
