26a706048dc3c2a4c2a84bfb71228ef4b0134504
tuyucheng <tuyucheng2000@163.com>
2025-09-27 11:25:31 +0800

    new module

---
+++ messaging-modules/dapr/dapr-subscriber/src/main/java/cn/tuyucheng/taketoday/dapr/pubsub/subscriber/DaprSubscriberApp.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.dapr.pubsub.subscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaprSubscriberApp {
+
+   public static void main(String[] args) {
+      SpringApplication.run(DaprSubscriberApp.class, args);
+   }
+}
