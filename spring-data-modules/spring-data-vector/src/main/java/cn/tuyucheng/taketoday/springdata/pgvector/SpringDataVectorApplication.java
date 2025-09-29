0d8a64cf2e77354c75b9ee4fbe3a51cd4105dcc7
tuyucheng <tuyucheng2000@163.com>
2025-09-29 13:06:51 +0800

    Intro to Repository Vector Search Methods

---
+++ spring-data-modules/spring-data-vector/src/main/java/cn/tuyucheng/taketoday/springdata/pgvector/SpringDataVectorApplication.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.springdata.pgvector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringDataVectorApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(SpringDataVectorApplication.class, args);
+   }
+}
