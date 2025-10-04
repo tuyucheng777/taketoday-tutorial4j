b9b9f23ace938bce312a76675ce26bb9c84cf861
tuyucheng <tuyucheng2000@163.com>
2025-10-04 20:46:50 +0800

    configuring multiple llms in spring-ai

---
+++ spring-ai-modules/spring-ai-multiple-llms/src/main/java/cn/tuyucheng/taketoday/multillm/Application.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.multillm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@SpringBootApplication
+class Application {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Application.class, args);
+   }
+}
