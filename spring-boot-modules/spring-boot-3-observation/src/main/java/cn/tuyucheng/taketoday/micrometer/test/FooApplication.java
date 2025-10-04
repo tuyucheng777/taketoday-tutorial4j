65d82c1b422f94a360f080478ebbf7a16b18c462
tuyucheng <tuyucheng2000@163.com>
2025-10-04 20:43:52 +0800

    testing micrometer

---
+++ spring-boot-modules/spring-boot-3-observation/src/main/java/cn/tuyucheng/taketoday/micrometer/test/FooApplication.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.micrometer.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(FooApplication.class, args);
+   }
+}
